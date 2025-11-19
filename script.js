9d642d725a0410c72a6c6300c668abc48e2f5806
andrezafigueiredo-tech <andreza.figueiredo@escola.pr.gov.br>
2025-11-18 21:10:20 -0300

    salvar

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let texto = document.querySelector("#texto")
-texto.textContent = "aluno!";
 
 let nome = prompt("Qual o seu nome?");
+if(nome == null){
+    texto.textContent = "Seja bem-vindo(a)!";
+}
+else{
+    texto.textContent = nome;
+}
+
+
